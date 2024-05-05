213c98bb3d5dba82837a66c7f2afe0750d5afb2c
alongw <alongw@alongw.cn>
2024-05-05 18:26:28 +0800

    feat(service): add get auth logs api

--- service/src/router/user/logs.ts
+++ service/src/router/user/logs.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { useLoginLogs } from '@/hook/useLogs'
+import { useLoginLogs, useAuthLogs } from '@/hook/useLogs'
 
 import { Request } from '@/types/request'
 
@@ -16,6 +16,30 @@ router.post(
         res
     ) => {
         const { getLogs } = useLoginLogs(req.user.uid)
+
+        // 默认获取 10 条记录
+        const max_number = req.body.max_number || 10
+
+        const { data, msg, status } = await getLogs(max_number)
+
+        return res.send({
+            status: status ? 200 : 500,
+            msg,
+            data
+        })
+    }
+)
+
+// 获取用户授权日志
+router.post(
+    '/logs',
+    async (
+        req: Request<{
+            max_number?: number
+        }>,
+        res
+    ) => {
+        const { getLogs } = useAuthLogs(req.user.uid)
 
         // 默认获取 10 条记录
         const max_number = req.body.max_number || 10
